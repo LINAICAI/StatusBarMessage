329b668205a2ba54c6b2baa6205ce783a853b53f
linaicai <linaicai2012@gmail.com>
2018-11-21 18:46:42 +0800

    0.1.1版本更新： 1、解决高度错误的问题 2、添加多一个loading样式，并且show调用之后可以拿到返回的实例，可以用来dismiss

--- Example/StatusBarMessage/ViewController.swift
+++ Example/StatusBarMessage/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import StatusBarMessage
+
 class ViewController: UIViewController {
 
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -34,16 +34,15 @@ class ViewController: UIViewController {
     }
     
     @objc func buttonClick(_ sender:UIButton) -> Void {
-        StatusBarMessage.show(with: "提交成功!", style: .success, duration: 2.0)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:{
-            StatusBarMessage.show(with: "请求失败!", style: .error, duration: 2.0)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:{
-                StatusBarMessage.show(with: "正在加载中...", style: .info, duration: 2.0)
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute:{
-                    StatusBarMessage.show(with: "网络状态不好...", style: .warning, duration: 2.0)
-                })
+
+        let message = StatusBarMessage.show(with: "正在加载中...", style: .loading)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute:{
+            message.dismiss({
+                StatusBarMessage.show(with: "提交成功!", style: .success, duration: 2.0)
             })
         })
+       
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- README.md
+++ README.md
@@ -3,8 +3,29 @@
 
 To run the example project, clone the repo, and run `pod install` from the Example directory first.
 
+## How to use
+
+```ruby
+StatusBarMessage.show(with: "submit success!", style: .success, duration: 2.0)
+```
+
+or if you want to show loading statusBar message and it should be dismiss with your custom block when you want to close , you should use follow : 
+
+```ruby
+let message = StatusBarMessage.show(with: "loading...", style: .loading)
+DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute:{
+//Do what you want to do
+message.dismiss({
+StatusBarMessage.show(with: "submit success!", style: .success, duration: 2.0)
+})
+})
+```
+
+
 ## Requirements
+
 iOS8 , Swift4
+
 ## Installation
 
 StatusBarMessage is available through [CocoaPods](https://cocoapods.org). To install
